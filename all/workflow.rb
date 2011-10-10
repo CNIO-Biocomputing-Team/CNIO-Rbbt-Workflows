53272bcf3cd719075badb83fd0be3c8b93e9ea2f
mikisvaz <miguel.vazquez@cnio.es>
2011-10-10 19:40:32 +0200

    Add Structure

--- all/workflow.rb
+++ all/workflow.rb
@@ -2,7 +2,9 @@ require 'rbbt/workflow'
 
 Workflow.require_workflow "Translation"
 Workflow.require_workflow "Sequence"
+Workflow.require_workflow "Structure"
+Workflow.require_workflow "Genotype"
 
-WorkflowREST.add_workflows Sequence, Translation
+WorkflowREST.add_workflows Sequence, Translation, Structure, Genotype
 WorkflowREST.setup
 
