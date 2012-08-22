2ae9c00189521cd22b6cba5044c74a5cedeb2522
Miguel Vazquez <miguel.vazquez@cnio.es>
2012-08-22 16:46:05 +0200

    nil

--- translation/lib/translation.rb
+++ translation/lib/translation.rb
@@ -5,9 +5,9 @@ module Translation
     @@index ||= {}
     if @@index[key].nil?
       if source.nil?
-        @@index[key] = Organism.identifiers(organism).index(:grep => "^LRG_", :invert_grep => true, :target => target, :persist => true, :order => true, :unnamed => true, :data_persist => true)
+        @@index[key] = Organism.identifiers(organism).index(:data_grep => "^LRG_", :data_invert_grep => true, :target => target, :persist => true, :order => true, :unnamed => true, :data_persist => true)
       else
-        @@index[key] = Organism.identifiers(organism).index(:grep => "^LRG_", :invert_grep => true, :target => target, :fields => [source], :persist => true, :order => true, :unnamed => true, :data_persist => true)
+        @@index[key] = Organism.identifiers(organism).index(:data_grep => "^LRG_", :data_invert_grep => true, :target => target, :fields => [source], :persist => true, :order => true, :unnamed => true, :data_persist => true)
       end
     end
     @@index[key]
@@ -18,9 +18,9 @@ module Translation
     @@protein_index ||= {}
     if @@protein_index[key].nil?
       if source.nil?
-        @@protein_index[key] = Organism.protein_identifiers(organism).index(:grep => "^LRG_", :invert_grep => true, :target => target, :persist => true, :order => true, :unnamed => true, :data_persist => true)
+        @@protein_index[key] = Organism.protein_identifiers(organism).index(:data_grep => "^LRG_", :data_invert_grep => true, :target => target, :persist => true, :order => true, :unnamed => true, :data_persist => true)
       else                                                                                                          
-        @@protein_index[key] = Organism.protein_identifiers(organism).index(:grep => "^LRG_", :invert_grep => true, :target => target, :fields => [source], :persist => true, :order => true, :unnamed => true, :data_persist => true)
+        @@protein_index[key] = Organism.protein_identifiers(organism).index(:data_grep => "^LRG_", :data_invert_grep => true, :target => target, :fields => [source], :persist => true, :order => true, :unnamed => true, :data_persist => true)
       end
       @@protein_index[key].unnamed = true
     end
@@ -32,9 +32,9 @@ module Translation
     @@probe_index ||= {}
     if @@probe_index[key].nil?
       if source.nil?
-        @@probe_index[key] = Organism.probe_transcripts(organism).index(:grep => "^LRG_", :invert_grep => true, :target => target, :persist => true, :order => true, :unnamed => true, :data_persist => true)
+        @@probe_index[key] = Organism.probe_transcripts(organism).index(:data_grep => "^LRG_", :data_invert_grep => true, :target => target, :persist => true, :order => true, :unnamed => true, :data_persist => true)
       else                                                                                                      
-        @@probe_index[key] = Organism.probe_transcripts(organism).index(:grep => "^LRG_", :invert_grep => true, :target => target, :fields => [source], :persist => true, :order => true, :unnamed => true, :data_persist => true)
+        @@probe_index[key] = Organism.probe_transcripts(organism).index(:data_grep => "^LRG_", :data_invert_grep => true, :target => target, :fields => [source], :persist => true, :order => true, :unnamed => true, :data_persist => true)
       end
       @@probe_index[key].unnamed = true
     end
@@ -45,7 +45,7 @@ module Translation
     key = [organism]
     @@transcript_to_protein_index ||= {}
     if @@transcript_to_protein_index[key].nil?
-      @@transcript_to_protein_index[key] = Organism.transcripts(organism).index(:grep => "^LRG_", :invert_grep => true, :target => "Ensembl Protein ID", :fields => ["Ensembl Transcript ID"], :persist => false, :unnamed => true, :data_persist => true)
+      @@transcript_to_protein_index[key] = Organism.transcripts(organism).index(:data_grep => "^LRG_", :data_invert_grep => true, :target => "Ensembl Protein ID", :fields => ["Ensembl Transcript ID"], :persist => false, :unnamed => true, :data_persist => true)
       @@transcript_to_protein_index[key].unnamed = true                                                                 
     end                                                                         
     @@transcript_to_protein_index[key]
