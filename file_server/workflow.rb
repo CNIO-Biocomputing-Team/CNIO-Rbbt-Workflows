2eb3a551773e000fc5f7d54e6c952974ca9735e8
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-01-14 19:46:40 +0100

    nil

--- file_server/workflow.rb
+++ file_server/workflow.rb
@@ -7,7 +7,7 @@ module FileServer
   input :resource, :string, "Resource that manages it", "Rbbt"
   input :produce, :boolean, "Produce if missing", false
   task :get_file => :binary do |file, resource, produce|
-    resource = "Rbbt" if resource.nil? or resource.empty?
+    raise "Please select a valid resource" if resource.nil? or resource.empty? or resource == "Rbbt"
     resource = Kernel.const_get(resource)
 
     path = resource.root[file]
@@ -19,6 +19,22 @@ module FileServer
 
     path.read
   end
-  
   export_exec :get_file
+ 
+  input :directory, :string, "Directory name to download"
+  input :resource, :string, "Resource that manages it", "Rbbt"
+  task :get_directory => :binary do |directory, resource|
+    raise "Please select a valid resource" if resource.nil? or resource.empty? or resource == "Rbbt"
+    resource = Kernel.const_get(resource)
+
+    path = resource.root[directory]
+
+    puts path
+
+    raise "Directory does not exist" unless path.exists?
+
+    CMD.cmd(" cd #{path.find}; tar cvfz - *", :pipe => true).read 
+  end
+  
+  export_synchronous :get_directory
 end
