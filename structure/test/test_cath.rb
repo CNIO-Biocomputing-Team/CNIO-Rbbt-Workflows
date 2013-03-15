d8c3a34956166ae7d6ac37d48b7875d7d4655c30
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-03-15 20:11:46 +0100

    nil

--- structure/test/test_cath.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/test_helper')
-require 'cath'
-
-class TestRbbt < Test::Unit::TestCase
-  def test_sequence
-    ddd Cath.align('1htoU01', "AAAATPDDVFKLAKDEKVEYVDVRFCDLPGIMQHFTIPASAFDKSVFDDGLAFDGSSIRGFQSIHESDMLLLPDPETARIDPFRAAKTLNINFFVHDPFTLEPYSRDAAAAAA")
-  end
-end
