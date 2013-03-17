489f40ff1c047a5b56ff0aa03562e99a980e6461
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-03-17 17:19:39 +0100

    nil

--- structure/lib/sinatra.rb
+++ structure/lib/sinatra.rb
@@ -3,7 +3,6 @@ require 'rbbt-util'
 Rbbt.claim Rbbt.www.views.public.js.jmol.find(:lib), :proc do |dir|
   url = "http://sourceforge.net/projects/jmol/files/Jmol/Version%2013.0/Version%2013.0.13/Jmol-13.0.13-binary.tar.gz/download"
   TmpFile.with_file do |zip_file|
-    ddd zip_file
     Open.write(zip_file, Open.read(url, :mode => 'rb'), :mode => 'wb')
     TmpFile.with_file do |unzip_dir|
       FileUtils.mkdir_p unzip_dir unless File.exists? unzip_dir
@@ -14,4 +13,3 @@ Rbbt.claim Rbbt.www.views.public.js.jmol.find(:lib), :proc do |dir|
   nil
 end
 
-Rbbt.www.views.public.js.jmol.find(:lib).produce
