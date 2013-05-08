bf83d2c886eee423b85a4ec0474df8aadd18f846
Miguel Vazquez <miguel.vazquez@cnio.es>
2013-05-08 12:39:52 +0200

    Moved here some property of mutated_isoforms

--- structure/workflow.rb
+++ structure/workflow.rb
@@ -222,3 +222,14 @@ module Structure
   end
   export_exec :sequence_position_in_pdb
 end
+
+if defined? Entity and defined? MutatedIsoform and Entity === MutatedIsoform
+  module MutatedIsoform
+    property :pdbs_and_positions => :single do
+      return [] if pdbs.nil?
+      pdbs.collect do |pdb, info|
+        [pdb, Structure.job(:sequence_position_in_pdb, "Protein: #{ self }", :sequence => protein.sequence, :organism => organism, :position => position, :pdb => pdb).run]
+      end
+    end
+  end
+end
